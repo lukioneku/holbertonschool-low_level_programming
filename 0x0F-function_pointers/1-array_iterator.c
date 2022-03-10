46ee4855e2b0923b94e672e269077d91e385906e
Yaniel <yanielddiaz@gmail.com>
2022-03-10 20:14:09 +0000

    making changes

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,23 +1,24 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
-  * array_iterator - ...
-  * @array: ...
-  * @size: ...
-  * @action: ...
-  *
-  * Return: ...
-  */
+ * array_iterator - executes a function given as a parameter on each element of
+ * an carray
+ * @size: size of the array
+ * @action: pointer to the function we need to use
+ * @array: array to pass
+ *
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int c;
 
-	if (array != NULL && action != NULL && size > 0)
+	if (array && action != NULL)
 	{
-		while (i < size)
+		for (c = 0; c < size; c++)
 		{
-			action(array[i]);
-			i++;
+			(action) (array[c]);
 		}
 	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,30 +1,30 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
-  * int_index - ...
-  * @array: ...
-  * @size: ...
-  * @cmp: ...
-  *
-  * Return: ...
-  */
+ * int_index - a function that searches for an integer
+ * @array: array to pass
+ * @size: number of elements in the array
+ * @cmp: pointer to the function to be used to compare values
+ *
+ * Return: the index of the first element for which the cmp function does not
+ * return 0. Otherwise, if no element matches or if size <= 0, return -1
+ */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int c;
 
-	if (size > 0)
+	if (size <= 0 || cmp == NULL || array == NULL)
 	{
-		if (array != NULL && cmp != NULL)
+		return (-1);
+	}
+	for (c = 0; c < size; c++)
+	{
+		if ((*cmp) (array[c]))
 		{
-			while (i < size)
-			{
-				if (cmp(array[i]))
-					return (i);
-
-				i++;
-			}
+			return (c);
 		}
 	}
-
 	return (-1);
 }
